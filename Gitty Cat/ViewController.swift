9521504e5b0932faf9ee491f4fc5b4602e1e0c43
Joseph Kurpinsky <jkurpinsky2022@208-Engineering-4.local>
2019-04-02 09:24:17 -0700

    Initial Commit

---
+++ Gitty Cat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitty Cat
+//
+//  Created by Joseph Kurpinsky on 4/2/19.
+//  Copyright © 2019 Potatoware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
